2cb0054a4782749dffd9ae086416bfe363ed2bfc
Tucker Klutey <kluteytk@amazon.com>
2019-11-09 19:44:03 -0500

    Adding soundfont dependency so piano plays sound
    Previously no impl to play sound so using soundfont lib to play MIDI sounds

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import Soundfont from 'soundfont-player';
 import 'react-piano/dist/styles.css';
 
 function App() {
+  const [piano, setPiano] = useState(null);
   const firstNote = MidiNumbers.fromNote('c3');
   const lastNote = MidiNumbers.fromNote('f5');
   const keyboardShortcuts = KeyboardShortcuts.create({
@@ -11,19 +13,25 @@ function App() {
     keyboardConfig: KeyboardShortcuts.HOME_ROW,
   });
 
+  Soundfont.instrument(new AudioContext(), 'acoustic_grand_piano').then(p => setPiano(p));
+
+    const pianoEl = !piano ? null :
+     (
+      <Piano
+        noteRange={{ first: firstNote, last: lastNote }}
+        playNote={(midiNumber) => {
+          piano.play(midiNumber)
+        }}
+        stopNote={(midiNumber) => {
+          // Stop playing a given note - see notes below
+        }}
+        width={1000}
+        keyboardShortcuts={keyboardShortcuts}
+      />
+    ) ;
   return (
-    <Piano
-      noteRange={{ first: firstNote, last: lastNote }}
-      playNote={(midiNumber) => {
-        // Play a given note - see notes below
-      }}
-      stopNote={(midiNumber) => {
-        // Stop playing a given note - see notes below
-      }}
-      width={1000}
-      keyboardShortcuts={keyboardShortcuts}
-    />
-  );
+    pianoEl
+  )
 }
 
 export default App;
